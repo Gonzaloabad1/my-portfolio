9d87469c072b436db612e3e4958275f0c2942054
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2025-03-26 12:08:33 +0100

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,17 @@
-import "./Footer.css";
-import { Button } from "../Button/Button";
-export const Footer = () => `
-<h2>Contact</h2>
-<div>
-${Button("/icons/twitter.png", "Twitter")}
-${Button("/icons/github.png", "GitHub")}
-${Button("/icons/linkedin.png", "LinkedIn")}
-${Button("/icons/telegram.png", "Telegram")}
-</div>
-`;
+import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function SocialLinks() {
+  return (
+    <div className="flex space-x-4">
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <FaTwitter className="w-8 h-8 text-blue-500 hover:text-blue-700" />
+      </a>
+      <a href="https://github.com/Gonzaloabad1" target="_blank" rel="noopener noreferrer">
+        <FaGithub className="w-8 h-8 text-gray-900 hover:text-gray-700" />
+      </a>
+      <a href="https://www.linkedin.com/in/gonzaloabadalonso/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin className="w-8 h-8 text-blue-700 hover:text-blue-900" />
+      </a>
+    </div>
+  );
+}
