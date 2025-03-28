20acc3f9e312e40bcecc5d1a0c58b7e7610e5f19
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2025-03-28 18:11:45 +0100

    Update Home.js

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -7,6 +7,6 @@ main.innerHTML = `
 <section class="home">
 <p>Hey, I'm</p>
 <h1>Gonzalo Abad</h1>
-<p>I'm a Fullstack designer & developer and writer born in Ciudad Real. I enjoy  creating fantastic things. what
+<p>I'm a Fullstack Designer & developer, writer born in Ciudad Real. I enjoy creating fantastic things.
 </section>`;
 };
