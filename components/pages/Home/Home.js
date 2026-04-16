b6193a0b4eb48d9f368efbcf43146d8ff2fe4677
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2026-04-16 18:48:25 +0200

    Update Home.js

--- components/pages/Home/Home.js
+++ components/pages/Home/Home.js
@@ -9,6 +9,6 @@ main.innerHTML = `
 <img src="8.jpg" alt="logo" style="width:200px;height:200px; border-radius: 50%;">
 <h1>Gonzalo Abad</h1>
 <p>I 'm a web developer and writer based in Ciudad Real, Spain. I enjoy creating incredibly wonderful things
-<a href="mailto: goabad18@outlook.com">Say hello →</a>
+<a href="https://gonzaloabad1.github.io/Web-gonzalo/">Say hello →</a>
 </section>`; ;
 };
