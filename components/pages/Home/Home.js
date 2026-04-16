9b7df35d1900fe5b63905d2d42104b25b5d030a6
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2026-04-16 18:52:07 +0200

    Update Home.js

--- components/pages/Home/Home.js
+++ components/pages/Home/Home.js
@@ -1,14 +1,21 @@
 import "./Home.css";
 import { cleanPage } from "../../utils/cleanPage";
+
 export const Home = () => {
-const main = document.querySelector("main");
-cleanPage(main);
-main.innerHTML = `
-<section class="home">
-<p>Hey, I'm</p>
-<img src="8.jpg" alt="logo" style="width:200px;height:200px; border-radius: 50%;">
-<h1>Gonzalo Abad</h1>
-<p>I 'm a web developer and writer based in Ciudad Real, Spain. I enjoy creating incredibly wonderful things
-<a href="https://gonzaloabad1.github.io/Web-gonzalo/">Say hello →</a>
-</section>`; ;
+  const main = document.querySelector("main");
+  
+  // Es buena práctica verificar si 'main' existe antes de limpiar
+  if (main) {
+    cleanPage(main);
+    main.innerHTML = `
+      <section class="home">
+        <p>Hey, I'm</p>
+        <img src="8.jpg" alt="Gonzalo Abad" class="home-profile-img">
+        <h1>Gonzalo Abad</h1>
+        <p>I'm a web developer and writer based in Ciudad Real, Spain. 
+           I enjoy creating incredibly wonderful things.</p>
+        <a href="https://gonzaloabad1.github.io/Web-gonzalo/" class="say-hello-link">Say hello →</a>
+      </section>
+    `;
+  }
 };
