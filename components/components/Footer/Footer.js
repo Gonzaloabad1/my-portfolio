4e7341b0a035c5eca0136dfc78e9b173edb97f8d
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2026-04-17 15:35:50 +0200

    Update Footer.js

--- components/components/Footer/Footer.js
+++ components/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import "./Footer.css";
 import { Button } from "../Button/Button";
 export const Footer = () => `
-<h2>Contact</h2>
 
 <h2 class="hover:text-gold transition-colors">
     <a href="https://gonzaloabad-design.vercel.app" target="_blank">Web</a>
