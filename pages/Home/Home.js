507d5137d43af467dacc8fbcbe375d53eb8fff63
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2026-06-08 12:56:43 +0200

    Add personal introduction and image to Home page
    Updated the Home.js file to include personal information and an image.
    
    Signed-off-by: Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -5,6 +5,7 @@ const main = document.querySelector("main");
 cleanPage(main);
 main.innerHTML = `
 <section class="home">
+<img src="https://pbs.twimg.com/profile_images/1454073836555620358/Lt2ZvywE_400x400.jpg" alt="Descripción de la imagen" width="500" height="300">
 <p>Hey, I'm</p>
 <h1>Gonzalo Abad</h1>
 <p>I'm a FullStack Designer and developer and writer born in Ciudad Real. I enjoy creating fanstastic things.<p> 
